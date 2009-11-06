fd8750a708a80967353be9092b34b55764ceaad7
Stuart Colville <muffinresearch@gmail.com>
2009-11-06 09:06:59 +0000

    started fleshing out Oauth

--- python-yql/yql/__init__.py
+++ python-yql/yql/__init__.py
@@ -1,9 +1,9 @@
-"""YQL Wrapper in Python
+"""python-yql
+
+YQL client for Python
 
 Stuart Colville
 http://muffinresearch.co.uk/
-
-BSD License
 
 from yql import YQL
 
@@ -11,36 +11,87 @@ from yql import YQL
 >>> query = 'select * from flickr.photos.search where text="panda" limit 3';
 >>> yql.execute(query)
 
+TODO: Ensure Two and three legged OAuth works
+TODO: Add support for parameterized Queries http://developer.yahoo.com/yql/guide/var_substitution.html
+
 """
 
 import os
 import sys
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
-
-from yql.oauth_client import YOAuthClient
-from urllib import urlencode
-from httplib2 import Http
+import urlparse, cgi
 
 try:
     import json
 except ImportError:
     import simplejson as json
 
+
+from urllib import urlencode
+from httplib2 import Http
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+from yql.oauth_client import YOAuthClient
+
 __author__ = 'Stuart Colville'
 __version__ = '0.1'
-
 __all__=['YQL']
-
 
 PUBLIC_URI = "http://query.yahooapis.com/v1/public/yql"
 PRIVATE_URI = "http://query.yahooapis.com/v1/yql"
+
+
+
 
 class YQL(object):
 
     def __init__(self, api_key=None, shared_secret=None, *args, **kwargs):
         self.api_key = api_key
-        shared_secret = shared_secret
+        self.secret = shared_secret
+
+
+    def access_protected(self, url):
+    
+        client = YOAuthClient(self.api_key, self.secret, url)
+        consumer = oauth.OAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET)
+        signature_method_plaintext = oauth.OAuthSignatureMethod_PLAINTEXT()
+        signature_method_hmac_sha1 = oauth.OAuthSignatureMethod_HMAC_SHA1()
+        pause()
+
+        # get request token
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(
+             consumer, callback=CALLBACK_URL, http_url=client.request_token_url)
+
+        oauth_request.sign_request(signature_method_plaintext, consumer, None)
+        token = client.fetch_request_token(oauth_request)
+
+        oauth_request = oauth.OAuthRequest.from_token_and_callback(
+                                 token=token, http_url=client.authorization_url)
+        
+        # this will actually occur only on some callback
+        response = client.authorize_token(oauth_request)
+        
+        # sad way to get the verifier
+        query = urlparse.urlparse(response)[4]
+        params = cgi.parse_qs(query, keep_blank_values=False)
+        verifier = params['oauth_verifier'][0]
+
+        # get access token
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
+               token=token, verifier=verifier, http_url=client.access_token_url)
+
+        oauth_request.sign_request(signature_method_plaintext, consumer, token)
+        token = client.fetch_access_token(oauth_request)
+
+        # access some protected resources
+        parameters = {'file': 'vacation.jpg', 'size': 'original'} # resource specific params
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, 
+                                token=token, http_method='POST', 
+                                http_url=RESOURCE_URL, parameters=parameters)
+        
+        oauth_request.sign_request(signature_method_hmac_sha1, consumer, token)
+        params = client.access_resource(oauth_request)
+        return params
+
     
     def execute(self, query, *args, **kwargs):
         """Run the YQL query"""
@@ -61,13 +112,11 @@ class YQL(object):
        
         # Need to carry out if we are using the private endpoint
         if uri == 'private':
-            # invoke Oauth
-            oa_client = YOAuthClient(self.api_key, 
-                                        self.shared_secret, call_back_url=None)
+            resp = self.access_protected('%s?%s' % (uri, query_string))
         else:
             h = Http()
             query_string = urlencode(params)
-            resp, content = h.request('%s?%s' % (PUBLIC_URI, query_string), "POST", query_string)
+            resp, content = h.request('%s?%s' % (uri, query_string), "POST", query_string)
 
         if resp.get('status') == '200':
             return json.loads(content)
@@ -76,8 +125,18 @@ class YQL(object):
 if __name__ == "__main__":
 
     yql = YQL()
+    print "Making Public Query"
     query = 'select * from flickr.photos.search where text="panda" limit 3';
     print yql.execute(query)
    
-
+    try:
+        print "Making Private Call"
+        from yql.keys import SECRET, API_KEY
+        yql = YQL(API_KEY, SECRET)
+        query = "SELECT * from geo.places WHERE text='SFO'"
+        print yql.execute(query, endpoint='private')
+    except ImportError:
+        print "You need a file containing your Secret and API key"
+ 
+   
     
--- python-yql/yql/oauth_client.py
+++ python-yql/yql/oauth_client.py
@@ -1,3 +1,20 @@
+"""
+
+Yahoo Specific client for handling two and three legged OAuth
+=============================================================
+
+Dependencies
+------------
+
+* python-oauth by leah Culver
+* httplib2 by Joe Gregorio
+
+Stuart Colville
+http://muffinresearch.co.uk/
+
+"""
+
+import httplib
 from oauth.oauth import OAuthClient
 
 class YOAuthClient(OAuthClient):
@@ -6,14 +23,35 @@ class YOAuthClient(OAuthClient):
     access_token_uri = 'https://api.login.yahoo.com/oauth/v2/get_token'
     auth_uri = 'https://api.login.yahoo.com/oauth/v2/request_auth'
 
+    def __init__(self, key, secret, url):
+        self.key = key
+        self.secret = secret
+        self.url = url
+        self.http = httplib.Http()
+    
     def fetch_request_token(self, oauth_request):
         """Get the request token."""
-        raise NotImplementedError
+        resp, content = self.http.request(self.request_token_url, 
+                                        headers=oauth_request.to_header()) 
+        return oauth.OAuthToken.from_string(content)
 
     def fetch_access_token(self, oauth_request):
         """Get the access token"""
-        raise NotImplementedError
+        resp, content = self.http.request(self.access_token_url, 
+                                        headers=oauth_request.to_header()) 
+        return oauth.OAuthToken.from_string(content)
+
+    def authorize_token(self, oauth_request):
+        """Authorize the token"""
+        resp,content = self.http.request(oauth_request.to_url()) 
+        return content 
 
     def access_resource(self, oauth_request):
-        """Some protected resource."""
-        raise NotImplementedError
+        """Access the protected resource."""
+        headers = {'Content-Type' :'application/x-www-form-urlencoded'}
+        resp, cont = self.http.request(self.url, 'POST', 
+                               oauth_request.to_postdata(), headers=headers)
+        return content
+
+
+
