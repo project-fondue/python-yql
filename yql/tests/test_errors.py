d8878b3b6a8ddeabb119b5f6ba91546f9e2f5fca
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-05-19 06:31:42 -0300

    Covering representation for NotOneError

--- yql/tests/test_errors.py
+++ yql/tests/test_errors.py
@@ -2,7 +2,7 @@ import json
 import os
 from unittest import TestCase
 
-from yql import YQLError
+from yql import NotOneError, YQLError
 
 
 class YQLErrorTest(TestCase):
@@ -21,3 +21,10 @@ class YQLErrorTest(TestCase):
 
         #TODO: verify if this is really the intended representation
         self.assertEqual("u'some description'", str(error))
+
+
+class NotOneErrorTest(TestCase):
+    def test_is_represented_by_message_as_json(self):
+        error = NotOneError('some message')
+
+        self.assertEqual("'some message'", str(error))
