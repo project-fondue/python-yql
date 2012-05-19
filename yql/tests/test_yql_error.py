a3b3dbbde95388cbf3f273ba0ed50c3fe2d452f2
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-05-19 06:10:35 -0300

    Covering YQLError

---
+++ yql/tests/test_yql_error.py
@@ -0,0 +1,23 @@
+import json
+import os
+from unittest import TestCase
+
+from yql import YQLError
+
+
+class YQLErrorTest(TestCase):
+    def test_is_represented_by_content_as_json_if_provided_as_string(self):
+        error = YQLError(resp='some response', content='some content')
+
+        self.assertEqual("'some content'", str(error))
+
+    def test_is_represented_by_error_description_as_json_if_provided_as_json(self):
+        content = {
+            'error': {
+                'description': 'some description',
+            }
+        }
+        error = YQLError(resp='some response', content=json.dumps(content))
+
+        #TODO: verify if this is really the intended representation
+        self.assertEqual("u'some description'", str(error))
