58cb06ce30aae2bba995abe48fd55cfacf57514a
Stuart Colville <muffinresearchlabs@gmail.com>
2012-05-22 11:50:25 +0100

    tidy up utils

--- yql/utils.py
+++ yql/utils.py
@@ -10,6 +10,7 @@ METHOD_MAP = (
 MULTI_PLUS = re.compile(r"\+{2,}")
 MULTI_SPACE = re.compile(r" {2,}")
 
+
 def get_http_method(query):
     """Work out if this should be GET, POST, PUT or DELETE"""
     lower_query = query.strip().lower()
@@ -22,15 +23,16 @@ def get_http_method(query):
 
     return http_method
 
+
 def clean_url(url):
     """Cleans up a uri/url"""
     url = url.replace("\n", "")
     url = MULTI_PLUS.sub("+", url)
     return url
 
+
 def clean_query(query):
     """Cleans up a query"""
     query = query.replace("\n", "")
     query = MULTI_SPACE.sub(" ", query)
     return query
-
