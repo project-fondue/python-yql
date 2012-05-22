d50801bdb1e552f75e2a755119283024728366eb
Stuart Colville <muffinresearchlabs@gmail.com>
2012-05-22 11:49:05 +0100

    clean-up test_yahoo_token

--- yql/tests/test_yahoo_token.py
+++ yql/tests/test_yahoo_token.py
@@ -12,12 +12,14 @@ def test_create_yahoo_token():
     assert token.key == 'test-key'
     assert token.secret == 'test-secret'
 
+
 def test_y_token_to_string():
     token = yql.YahooToken('test-key', 'test-secret')
     token_to_string = token.to_string()
     string_data = dict(parse_qsl(token_to_string))
     assert string_data.get('oauth_token') == 'test-key'
     assert string_data.get('oauth_token_secret') == 'test-secret'
+
 
 def test_y_token_to_string2():
     token = yql.YahooToken('test-key', 'test-secret')
@@ -34,35 +36,43 @@ def test_y_token_to_string2():
     assert string_data.get('oauth_callback_confirmed') == 'basilfawlty'
     assert string_data.get('oauth_session_handle') == 'poop'
 
+
 def test_y_token_from_string():
-    token_string = "oauth_token=foo&oauth_token_secret=bar&oauth_session_handle=baz&token_creation_timestamp=1111"
+    token_string = "oauth_token=foo&oauth_token_secret=bar&"\
+                   "oauth_session_handle=baz&token_creation_timestamp=1111"
     token_from_string = yql.YahooToken.from_string(token_string)
     assert token_from_string.key == 'foo'
     assert token_from_string.secret == 'bar'
     assert token_from_string.session_handle == 'baz'
     assert token_from_string.timestamp == '1111'
 
+
 @raises(ValueError)
 def test_y_token_raises_value_error():
     yql.YahooToken.from_string('')
+
 
 @raises(ValueError)
 def test_y_token_raises_value_error2():
     yql.YahooToken.from_string('foo')
 
+
 @raises(ValueError)
 def test_y_token_raises_value_error3():
     yql.YahooToken.from_string('oauth_token=bar')
 
+
 @raises(ValueError)
 def test_y_token_raises_value_error4():
     yql.YahooToken.from_string('oauth_token_secret=bar')
+
 
 @raises(AttributeError)
 def test_y_token_without_timestamp_raises():
     token = yql.YahooToken('test', 'test2')
     y = yql.ThreeLegged('test', 'test2')
     y.check_token(token)
+
 
 def test_y_token_without_timestamp_raises2():
 
@@ -75,5 +85,3 @@ def test_y_token_without_timestamp_raises2():
     token = yql.YahooToken('test', 'test2')
     token.timestamp = 11111
     assert y.check_token(token) == 'replaced'
-
-
