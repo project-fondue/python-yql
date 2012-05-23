1dd2f4d648ed6f89689e189fc6acabe6679d7b30
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-05-23 02:04:41 -0300

    Migrating YahooToken tests to unittest

--- yql/tests/test_yahoo_token.py
+++ yql/tests/test_yahoo_token.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 import yql
 from nose.tools import raises
 
@@ -7,81 +9,73 @@ except ImportError:
     from cgi import parse_qs, parse_qsl
 
 
-def test_create_yahoo_token():
-    token = yql.YahooToken('test-key', 'test-secret')
-    assert token.key == 'test-key'
-    assert token.secret == 'test-secret'
+class YahooTokenTest(TestCase):
+    def test_create_yahoo_token(self):
+        token = yql.YahooToken('test-key', 'test-secret')
+        self.assertEqual(token.key, 'test-key')
+        self.assertEqual(token.secret, 'test-secret')
 
+    def test_y_token_to_string(self):
+        token = yql.YahooToken('test-key', 'test-secret')
+        token_to_string = token.to_string()
+        string_data = dict(parse_qsl(token_to_string))
+        self.assertEqual(string_data.get('oauth_token'), 'test-key')
+        self.assertEqual(string_data.get('oauth_token_secret'), 'test-secret')
 
-def test_y_token_to_string():
-    token = yql.YahooToken('test-key', 'test-secret')
-    token_to_string = token.to_string()
-    string_data = dict(parse_qsl(token_to_string))
-    assert string_data.get('oauth_token') == 'test-key'
-    assert string_data.get('oauth_token_secret') == 'test-secret'
+    def test_y_token_to_string2(self):
+        token = yql.YahooToken('test-key', 'test-secret')
 
+        token.timestamp = '1111'
+        token.session_handle = 'poop'
+        token.callback_confirmed = 'basilfawlty'
 
-def test_y_token_to_string2():
-    token = yql.YahooToken('test-key', 'test-secret')
+        token_to_string = token.to_string()
+        string_data = dict(parse_qsl(token_to_string))
+        self.assertEqual(string_data.get('oauth_token'), 'test-key')
+        self.assertEqual(string_data.get('oauth_token_secret'), 'test-secret')
+        self.assertEqual(string_data.get('token_creation_timestamp'), '1111')
+        self.assertEqual(string_data.get('oauth_callback_confirmed'), 'basilfawlty')
+        self.assertEqual(string_data.get('oauth_session_handle'), 'poop')
 
-    token.timestamp = '1111'
-    token.session_handle = 'poop'
-    token.callback_confirmed = 'basilfawlty'
+    def test_y_token_from_string(self):
+        token_string = "oauth_token=foo&oauth_token_secret=bar&"\
+                       "oauth_session_handle=baz&token_creation_timestamp=1111"
+        token_from_string = yql.YahooToken.from_string(token_string)
+        self.assertEqual(token_from_string.key, 'foo')
+        self.assertEqual(token_from_string.secret, 'bar')
+        self.assertEqual(token_from_string.session_handle, 'baz')
+        self.assertEqual(token_from_string.timestamp, '1111')
 
-    token_to_string = token.to_string()
-    string_data = dict(parse_qsl(token_to_string))
-    assert string_data.get('oauth_token') == 'test-key'
-    assert string_data.get('oauth_token_secret') == 'test-secret'
-    assert string_data.get('token_creation_timestamp') == '1111'
-    assert string_data.get('oauth_callback_confirmed') == 'basilfawlty'
-    assert string_data.get('oauth_session_handle') == 'poop'
+    @raises(ValueError)
+    def test_y_token_raises_value_error(self):
+        yql.YahooToken.from_string('')
 
+    @raises(ValueError)
+    def test_y_token_raises_value_error2(self):
+        yql.YahooToken.from_string('foo')
 
-def test_y_token_from_string():
-    token_string = "oauth_token=foo&oauth_token_secret=bar&"\
-                   "oauth_session_handle=baz&token_creation_timestamp=1111"
-    token_from_string = yql.YahooToken.from_string(token_string)
-    assert token_from_string.key == 'foo'
-    assert token_from_string.secret == 'bar'
-    assert token_from_string.session_handle == 'baz'
-    assert token_from_string.timestamp == '1111'
+    @raises(ValueError)
+    def test_y_token_raises_value_error3(self):
+        yql.YahooToken.from_string('oauth_token=bar')
 
+    @raises(ValueError)
+    def test_y_token_raises_value_error4(self):
+        yql.YahooToken.from_string('oauth_token_secret=bar')
 
-@raises(ValueError)
-def test_y_token_raises_value_error():
-    yql.YahooToken.from_string('')
+    @raises(AttributeError)
+    def test_y_token_without_timestamp_raises(self):
+        token = yql.YahooToken('test', 'test2')
+        y = yql.ThreeLegged('test', 'test2')
+        y.check_token(token)
 
+    def test_y_token_without_timestamp_raises2(self):
 
-@raises(ValueError)
-def test_y_token_raises_value_error2():
-    yql.YahooToken.from_string('foo')
+        def refresh_token_replacement(token):
+            return 'replaced'
 
+        y = yql.ThreeLegged('test', 'test2')
+        y.refresh_token = refresh_token_replacement
 
-@raises(ValueError)
-def test_y_token_raises_value_error3():
-    yql.YahooToken.from_string('oauth_token=bar')
-
-
-@raises(ValueError)
-def test_y_token_raises_value_error4():
-    yql.YahooToken.from_string('oauth_token_secret=bar')
-
-
-@raises(AttributeError)
-def test_y_token_without_timestamp_raises():
-    token = yql.YahooToken('test', 'test2')
-    y = yql.ThreeLegged('test', 'test2')
-    y.check_token(token)
-
-
-def test_y_token_without_timestamp_raises2():
-
-    def refresh_token_replacement(token):
-        return 'replaced'
-
-    y = yql.ThreeLegged('test', 'test2')
-    y.refresh_token = refresh_token_replacement
-
-    token = yql.YahooToken('test', 'test2')
-    token.timestamp = 11111
-    assert y.check_token(token) == 'replaced'
+        token = yql.YahooToken('test', 'test2')
+        token.timestamp = 11111
+        self.assertEqual(y.check_token(token), 'replaced')
