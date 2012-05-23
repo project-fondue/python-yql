978c0a2bea8c653ae3b203a8cacc5da68250f58e
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-05-23 02:00:31 -0300

    Separating get_http_method tests in different test cases

--- yql/tests/test_utilities.py
+++ yql/tests/test_utilities.py
@@ -1,27 +1,69 @@
+from unittest import TestCase
+
 from yql.utils import get_http_method
 
-TEST_DATA = (
-    (" SELECT foo", "GET"),
-    ("SELECT foo", "GET"),
-    ("select foo", "GET"),
-    ("INSERT into", "POST"),
-    ("INSERT into", "POST"),
-    ("update foo", "PUT"),
-    ("DELETE from", "POST"),
-    ("delete from", "POST"),
-    ("delete from", "POST"),
-    ("""INSERT INTO yql.queries.query (name, query)
+
+class UtilitiesTest(TestCase):
+    def test_finds_get_method_for_select_query(self):
+        query = "SELECT foo"
+        expected_verb = "GET"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_get_method_for_select_query_with_leading_space(self):
+        query = " SELECT foo"
+        expected_verb = "GET"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_get_method_for_lowercase_select_query(self):
+        query = "select foo"
+        expected_verb = "GET"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_post_method_for_insert_query(self):
+        query = "INSERT into"
+        expected_verb = "POST"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_post_method_for_multiline_insert_query(self):
+        query = """
+        INSERT INTO yql.queries.query (name, query)
         VALUES ("weather", "SELECT * FROM weather.forecast
-            WHERE location=90210")""", "POST"),
-    ("SHOW tables", "GET"),
-    ("DESC tablename", "GET"),
-)
+            WHERE location=90210")
+            """
+        expected_verb = "POST"
 
+        self.assertEqual(get_http_method(query), expected_verb)
 
-def test_verb_from_query():
-    for query, verb in TEST_DATA:
-        yield find_verb_from_query, query, verb
+    def test_finds_put_method_for_update_query(self):
+        query = "update foo"
+        expected_verb = "PUT"
 
+        self.assertEqual(get_http_method(query), expected_verb)
 
-def find_verb_from_query(query, verb):
-    assert get_http_method(query) == verb
+    def test_finds_post_method_for_delete_query(self):
+        query = "DELETE from"
+        expected_verb = "POST"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_post_method_for_lowercase_delete_query(self):
+        query = "delete from"
+        expected_verb = "POST"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_get_method_for_show_query(self):
+        query = "SHOW tables"
+        expected_verb = "GET"
+
+        self.assertEqual(get_http_method(query), expected_verb)
+
+    def test_finds_get_method_for_describe_query(self):
+        query = "DESC tablename"
+        expected_verb = "GET"
+
+        self.assertEqual(get_http_method(query), expected_verb)
