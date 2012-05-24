0590e336887ac637497e7be6a3efc5602a963203
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-05-24 00:44:40 -0300

    Simplifying UtilitiesTest (removing uneeded blank lines and explanation variables)

--- yql/tests/test_utilities.py
+++ yql/tests/test_utilities.py
@@ -5,28 +5,16 @@ from yql.utils import get_http_method
 
 class UtilitiesTest(TestCase):
     def test_finds_get_method_for_select_query(self):
-        query = "SELECT foo"
-        expected_verb = "GET"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("SELECT foo"), "GET")
 
     def test_finds_get_method_for_select_query_with_leading_space(self):
-        query = " SELECT foo"
-        expected_verb = "GET"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method(" SELECT foo"), "GET")
 
     def test_finds_get_method_for_lowercase_select_query(self):
-        query = "select foo"
-        expected_verb = "GET"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("select foo"), "GET")
 
     def test_finds_post_method_for_insert_query(self):
-        query = "INSERT into"
-        expected_verb = "POST"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("INSERT into"), "POST")
 
     def test_finds_post_method_for_multiline_insert_query(self):
         query = """
@@ -34,36 +22,19 @@ class UtilitiesTest(TestCase):
         VALUES ("weather", "SELECT * FROM weather.forecast
             WHERE location=90210")
             """
-        expected_verb = "POST"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method(query), "POST")
 
     def test_finds_put_method_for_update_query(self):
-        query = "update foo"
-        expected_verb = "PUT"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("update foo"), "PUT")
 
     def test_finds_post_method_for_delete_query(self):
-        query = "DELETE from"
-        expected_verb = "POST"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("DELETE from"), "POST")
 
     def test_finds_post_method_for_lowercase_delete_query(self):
-        query = "delete from"
-        expected_verb = "POST"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("delete from"), "POST")
 
     def test_finds_get_method_for_show_query(self):
-        query = "SHOW tables"
-        expected_verb = "GET"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("SHOW tables"), "GET")
 
     def test_finds_get_method_for_describe_query(self):
-        query = "DESC tablename"
-        expected_verb = "GET"
-
-        self.assertEqual(get_http_method(query), expected_verb)
+        self.assertEqual(get_http_method("DESC tablename"), "GET")
