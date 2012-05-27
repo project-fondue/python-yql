ba82cc64702ca4b09242c58aca6620b7d13745fb
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-05-27 00:17:07 -0300

    Finishing migration of TwoLegged and ThreeLegged tests to unittest.TestCase

--- yql/tests/test_requests_responses.py
+++ yql/tests/test_requests_responses.py
@@ -109,34 +109,44 @@ class TestThreeLegged(yql.ThreeLegged):
     execute = execute_return_uri
 
 
-class PublicTest(TestCase):
-    @with_setup(set_up_http_request_data, tear_down_http_request_data)
+class PublicStubbedRequestTest(TestCase):
+    def setUp(self):
+        set_up_http_request_data()
+
+    def tearDown(self):
+        tear_down_http_request_data()
+
     def test_urlencoding_for_public_yql(self):
         query = 'SELECT * from foo'
         y = TestPublic(httplib2_inst=httplib2.Http())
         uri = y.execute(query)
         self.assertEqual(uri, "http://query.yahooapis.com/v1/public/yql?q=SELECT+%2A+from+foo&format=json")
 
-    @with_setup(set_up_http_request_data, tear_down_http_request_data)
     def test_env_for_public_yql(self):
         query = 'SELECT * from foo'
         y = TestPublic(httplib2_inst=httplib2.Http())
         uri = y.execute(query, env="http://foo.com")
         self.assertGreater(uri.find(urlencode({"env":"http://foo.com"})), -1)
 
-    @with_setup(set_up_http_request_data, tear_down_http_request_data)
     def test_name_param_inserted_for_public_yql(self):
         query = 'SELECT * from foo WHERE dog=@dog'
         y = TestPublic(httplib2_inst=httplib2.Http())
         uri = y.execute(query, {"dog": "fifi"})
         self.assertGreater(uri.find('dog=fifi'), -1)
 
-        @with_setup(set_up_http_from_file, tear_down_http_from_file)
-        def test_json_response_from_file(self):
-            query = 'SELECT * from foo WHERE dog=@dog'
-            y = yql.Public(httplib2_inst=httplib2.Http())
-            content = y.execute(query, {"dog": "fifi"})
-            self.assertEqual(content.count, 3)
+
+class PublicStubbedFromFileTest(TestCase):
+    def setUp(self):
+        set_up_http_from_file()
+
+    def tearDown(self):
+        tear_down_http_from_file()
+
+    def test_json_response_from_file(self):
+        query = 'SELECT * from foo WHERE dog=@dog'
+        y = yql.Public(httplib2_inst=httplib2.Http())
+        content = y.execute(query, {"dog": "fifi"})
+        self.assertEqual(content.count, 3)
 
 
 class TwoLeggedTest(TestCase):
@@ -144,108 +154,134 @@ class TwoLeggedTest(TestCase):
     def test_yql_with_2leg_auth_raises_typerror(self):
         TestTwoLegged()
 
+    def test_api_key_and_secret_attrs(self):
+        y = yql.TwoLegged('test-api-key', 'test-secret')
+        assert y.api_key == 'test-api-key'
+        assert y.secret == 'test-secret'
+
+    def test_get_two_legged_request_keys(self):
+        y = yql.TwoLegged('test-api-key', 'test-secret')
+        # Accessed this was because it's private
+        request =  y._TwoLegged__two_legged_request('http://google.com')
+        assert set(['oauth_nonce', 'oauth_version', 'oauth_timestamp',
+            'oauth_consumer_key', 'oauth_signature_method', 'oauth_body_hash',
+            'oauth_version', 'oauth_signature']) == set(request.keys())
+
+    def test_get_two_legged_request_values(self):
+        y = yql.TwoLegged('test-api-key', 'test-secret')
+        # Accessed this was because it's private
+        request =  y._TwoLegged__two_legged_request('http://google.com')
+        assert request['oauth_consumer_key'] == 'test-api-key'
+        assert request['oauth_signature_method'] == 'HMAC-SHA1'
+        assert request['oauth_version'] == '1.0'
+
+    def test_get_two_legged_request_param(self):
+        y = yql.TwoLegged('test-api-key', 'test-secret')
+        # Accessed this way because it's private
+        request =  y._TwoLegged__two_legged_request('http://google.com',
+                                                            {"test-param": "test"})
+        assert request.get('test-param') == 'test'
+
+
+class TwoLeggedStubbedRequestTest(TestCase):
+    def setUp(self):
+        set_up_http_request_data()
+
+    def tearDown(self):
+        tear_down_http_request_data()
+
+    def test_request_for_two_legged(self):
+        query = 'SELECT * from foo'
+        y = TestTwoLegged('test-api-key', 'test-secret', httplib2_inst=httplib2.Http())
+        signed_url = y.execute(query)
+        qs  = dict(parse_qsl(signed_url.split('?')[1]))
+        assert qs['q'] == query
+        assert qs['format'] == 'json'
+
+
+class TwoLeggedStubbedFromFileTest(TestCase):
+    def setUp(self):
+        set_up_http_from_file()
+
+    def tearDown(self):
+        tear_down_http_from_file()
+
+    def test_get_two_legged_from_file(self):
+        query = 'SELECT * from foo'
+        y = yql.TwoLegged('test-api-key', 'test-secret', httplib2_inst=httplib2.Http())
+        # Accessed this was because it's private
+        assert y.execute(query) is not None
+
 
 class ThreeLeggedTest(TestCase):
     @raises(TypeError)
-    def test_yql_with_3leg_auth_raises_typerror():
+    def test_yql_with_3leg_auth_raises_typerror(self):
         TestThreeLegged()
 
-def test_api_key_and_secret_attrs():
-    y = yql.TwoLegged('test-api-key', 'test-secret')
-    assert y.api_key == 'test-api-key'
-    assert y.secret == 'test-secret'
+    def test_api_key_and_secret_attrs2(self):
+        y = yql.ThreeLegged('test-api-key', 'test-secret')
+        assert y.api_key == 'test-api-key'
+        assert y.secret == 'test-secret'
 
-def test_api_key_and_secret_attrs2():
-    y = yql.ThreeLegged('test-api-key', 'test-secret')
-    assert y.api_key == 'test-api-key'
-    assert y.secret == 'test-secret'
+    def test_get_base_params(self):
+        y = yql.ThreeLegged('test-api-key', 'test-secret')
+        result = y.get_base_params()
+        assert set(['oauth_nonce', 'oauth_version', 'oauth_timestamp']) \
+                                                        == set(result.keys())
 
-def test_get_base_params():
-    y = yql.ThreeLegged('test-api-key', 'test-secret')
-    result = y.get_base_params()
-    assert set(['oauth_nonce', 'oauth_version', 'oauth_timestamp']) \
-                                                    == set(result.keys())
+    @raises(ValueError)
+    def test_raises_for_three_legged_with_no_token(self):
+        query = 'SELECT * from foo'
+        y = TestThreeLegged('test-api-key', 'test-secret', httplib2_inst=httplib2.Http())
+        y.execute(query)
 
-def test_get_two_legged_request_keys():
-    y = yql.TwoLegged('test-api-key', 'test-secret')
-    # Accessed this was because it's private
-    request =  y._TwoLegged__two_legged_request('http://google.com')
-    assert set(['oauth_nonce', 'oauth_version', 'oauth_timestamp',
-        'oauth_consumer_key', 'oauth_signature_method', 'oauth_body_hash',
-        'oauth_version', 'oauth_signature']) == set(request.keys())
 
-def test_get_two_legged_request_values():
-    y = yql.TwoLegged('test-api-key', 'test-secret')
-    # Accessed this was because it's private
-    request =  y._TwoLegged__two_legged_request('http://google.com')
-    assert request['oauth_consumer_key'] == 'test-api-key'
-    assert request['oauth_signature_method'] == 'HMAC-SHA1'
-    assert request['oauth_version'] == '1.0'
+class ThreeLeggedStubbedRequestTest(TestCase):
+    def setUp(self):
+        set_up_http_request_data()
 
-def test_get_two_legged_request_param():
-    y = yql.TwoLegged('test-api-key', 'test-secret')
-    # Accessed this way because it's private
-    request =  y._TwoLegged__two_legged_request('http://google.com',
-                                                        {"test-param": "test"})
-    assert request.get('test-param') == 'test'
+    def tearDown(self):
+        tear_down_http_request_data()
 
-@with_setup(set_up_http_from_file, tear_down_http_from_file)
-def test_get_two_legged_from_file():
-    query = 'SELECT * from foo'
-    y = yql.TwoLegged('test-api-key', 'test-secret', httplib2_inst=httplib2.Http())
-    # Accessed this was because it's private
-    assert y.execute(query) is not None
+    def test_request_for_three_legged(self):
+        query = 'SELECT * from foo'
+        y = TestThreeLegged('test-api-key', 'test-secret',
+                                            httplib2_inst=httplib2.Http())
+        token = oauth.Token.from_string(
+                            'oauth_token=foo&oauth_token_secret=bar')
+        signed_url = y.execute(query, token=token)
+        qs  = dict(parse_qsl(signed_url.split('?')[1]))
+        assert qs['q'] == query
+        assert qs['format'] == 'json'
 
-@with_setup(set_up_http_request_data, tear_down_http_request_data)
-def test_request_for_two_legged():
-    query = 'SELECT * from foo'
-    y = TestTwoLegged('test-api-key', 'test-secret', httplib2_inst=httplib2.Http())
-    signed_url = y.execute(query)
-    qs  = dict(parse_qsl(signed_url.split('?')[1]))
-    assert qs['q'] == query
-    assert qs['format'] == 'json'
 
-@raises(ValueError)
-def test_raises_for_three_legged_with_no_token():
-    query = 'SELECT * from foo'
-    y = TestThreeLegged('test-api-key', 'test-secret', httplib2_inst=httplib2.Http())
-    y.execute(query)
+class ThreeLeggedStubbedFromFileTest(TestCase):
+    def setUp(self):
+        set_up_http_from_file()
 
-@with_setup(set_up_http_request_data, tear_down_http_request_data)
-def test_request_for_three_legged():
-    query = 'SELECT * from foo'
-    y = TestThreeLegged('test-api-key', 'test-secret',
-                                        httplib2_inst=httplib2.Http())
-    token = oauth.Token.from_string(
-                        'oauth_token=foo&oauth_token_secret=bar')
-    signed_url = y.execute(query, token=token)
-    qs  = dict(parse_qsl(signed_url.split('?')[1]))
-    assert qs['q'] == query
-    assert qs['format'] == 'json'
+    def tearDown(self):
+        tear_down_http_from_file()
 
-@with_setup(set_up_http_from_file, tear_down_http_from_file)
-def test_three_legged_execution():
-    query = 'SELECT * from foo WHERE dog=@dog'
-    y = yql.ThreeLegged('test','test2', httplib2_inst=httplib2.Http())
-    token = yql.YahooToken('test', 'test2')
-    content = y.execute(query, {"dog": "fifi"}, token=token)
-    assert content.count == 3
+    def test_three_legged_execution(self):
+        query = 'SELECT * from foo WHERE dog=@dog'
+        y = yql.ThreeLegged('test','test2', httplib2_inst=httplib2.Http())
+        token = yql.YahooToken('test', 'test2')
+        content = y.execute(query, {"dog": "fifi"}, token=token)
+        assert content.count == 3
 
-@raises(ValueError)
-@with_setup(set_up_http_from_file, tear_down_http_from_file)
-def test_three_legged_execution_raises_value_error_with_invalid_uri():
-    y = yql.ThreeLegged('test','test2', httplib2_inst=httplib2.Http())
-    y.uri = "fail"
-    token = yql.YahooToken('tes1t', 'test2')
-    y.execute("SELECT foo meh meh ", token=token)
+    @raises(ValueError)
+    def test_three_legged_execution_raises_value_error_with_invalid_uri(self):
+        y = yql.ThreeLegged('test','test2', httplib2_inst=httplib2.Http())
+        y.uri = "fail"
+        token = yql.YahooToken('tes1t', 'test2')
+        y.execute("SELECT foo meh meh ", token=token)
 
-@with_setup(set_up_http_from_file, tear_down_http_from_file)
-def test_get_access_token_request3():
-    y = yql.ThreeLegged('test', 'test-does-not-exist',
-                                httplib2_inst=httplib2.Http())
-    new_token = yql.YahooToken('test', 'test2')
-    new_token.session_handle = 'sess_handle_test'
-    token = y.refresh_token(token=new_token)
-    assert hasattr(token, 'key')
-    assert hasattr(token, 'secret')
+    def test_get_access_token_request3(self):
+        y = yql.ThreeLegged('test', 'test-does-not-exist',
+                                    httplib2_inst=httplib2.Http())
+        new_token = yql.YahooToken('test', 'test2')
+        new_token.session_handle = 'sess_handle_test'
+        token = y.refresh_token(token=new_token)
+        assert hasattr(token, 'key')
+        assert hasattr(token, 'secret')
 
